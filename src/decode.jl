d763d2d34e18731c84c42772a3a4b118972190c8
Al Asaad <alahmadgaid@gmail.com>
2021-01-16 14:18:06 +0800

    extended options for verse

--- src/decode.jl
+++ src/decode.jl
@@ -30,10 +30,10 @@ end
 
 Extract the verses of a `AbstractQuran` object.
 """
-function verses(quran::AbstractQuran)
+function verses(quran::AbstractQuran; number=false, start_end=true)
     try
         endidx = length(rows(quran.data))
-        return verses(quran.data, 1, endidx)
+        return verses(quran.data, 1, endidx; number=number, start_end=start_end)
     catch
         return select(quran.data, :form)
     end
@@ -53,37 +53,136 @@ end
 
 Extract the verses of a `IndexedTable` object from row `a` to row `b`.
 """
-function verses(data::IndexedTable, a::Int64, b::Int64)
-    verses = String[]; words = ""; j = 1
+function verses(data::IndexedTable, a::Int64, b::Int64; number::Bool=false, start_end::Bool=true)
+    if number
+        if start_end
+            verse_out = String[]
+            verse_nos = ""
+        else
+            verse_out = Tuple{Array{Int64,1},Array{Int64,1}}[]
+            chapter_nos = Int64[] 
+            verse_nos = Int64[]
+        end
+    else
+        verse_out = String[]; words = ""
+    end
+
+    j = 1
     for i in select(data, :form)[a:b]
         if j > 1
             prev_word  = select(data, :word)[j-1]
             next_word  = select(data, :word)[j]
             prev_verse = select(data, :verse)[j-1]
             next_verse = select(data, :verse)[j]
+            prev_chapter = select(data, :chapter)[j-1]
+            next_chapter = select(data, :chapter)[j]
             
             if prev_word !== next_word && prev_verse !== next_verse
-                push!(verses, words)
-                words = ""
+                if number
+                    if prev_chapter !== next_chapter
+                        if start_end
+                            push!(verse_out, verse_nos * ")")
+                            verse_nos = string(next_chapter) * ":(" * string(next_verse)
+                        else
+                            push!(verse_out, (chapter_nos, verse_nos))
+                            chapter_nos = Int64[] 
+                            verse_nos = Int64[]
+
+                            push!(chapter_nos, next_chapter)
+                            push!(verse_nos, next_verse)
+                        end
+                    else
+                        if start_end
+                            if occursin(",", verse_nos)
+                                verse_nos = replace(verse_nos, "," * string(prev_verse) => "," * string(next_verse))
+                            else
+                                verse_nos *= "," * string(next_verse)
+                            end
+                        else
+                            push!(verse_nos, next_verse)
+                        end
+                    end
+                else
+                    push!(verse_out, words)
+                    words = ""
+                end
             elseif prev_word !== next_word && prev_verse === next_verse
-                words *= " "
+                if !number
+                    words *= " "
+                end
             elseif prev_word === next_word && prev_verse !== next_verse
-                push!(verses, words)
-                words = ""
+                if number 
+                    if prev_chapter !== next_chapter
+                        if start_end
+                            push!(verse_out, verse_nos * ")")
+                            verse_nos = string(next_chapter) * ":(" * string(next_verse)
+                        else
+                            push!(verse_out, (chapter_nos, verse_nos))
+                            chapter_nos = Int64[] 
+                            verse_nos = Int64[]
+
+                            push!(chapter_nos, next_chapter)
+                            push!(verse_nos, next_verse)
+                        end
+                    else
+                        if start_end
+                            if occursin(",", verse_nos)
+                                verse_nos = replace(verse_nos, "," * string(prev_verse) => "," * string(next_verse))
+                            else
+                                verse_nos *= "," * string(next_verse)
+                            end
+                        else
+                            push!(verse_nos, next_verse)
+                        end
+                    end
+                else
+                    push!(verse_out, words)
+                    words = ""
+                end
             end
-            words *= i 
+
+            if !number
+                words *= i 
+            end
+
             if j === b
-                push!(verses, words)
+                if number
+                    if start_end
+                        push!(verse_out, verse_nos * ")")
+                    else
+                        push!(verse_out, (chapter_nos, verse_nos))
+                    end
+                else
+                    push!(verse_out, words)
+                end
             end
         else 
-            words *= i
+            if number
+                if start_end
+                    verse_nos = string(select(data, :chapter)[j]) * ":(" * string(select(data, :verse)[j])
+                else
+                    push!(chapter_nos, select(data, :chapter)[j])
+                    push!(verse_nos, select(data, :verse)[j])
+                end
+            else
+                words *= i
+            end
+
             if j === b
-                push!(verses, words)
+                if number
+                    if start_end
+                        push!(verse_out, verse_nos * ")")
+                    else
+                        push!(verse_out, (chapter_nos, verse_nos))
+                    end
+                else
+                    push!(verse_out, words)
+                end
             end
         end
         j += 1
     end
-    return verses
+    return verse_out
 end
 
 """
